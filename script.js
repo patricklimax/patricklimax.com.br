5d0a71aba7ac381854e639b249980a6f452c026b
patricklimax <patricklimax@outlook.com.br>
2022-09-06 15:01:38 -0300

    NovasCores

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+
+// js menu mobile
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+
+  nav.classList.toggle('active');
+
+  const active = nav.classList.contains('active');
+
+  event.currentTarget.setAttribute('aria-expanded', active);
+  
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+// fim js menu mobile
+
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
