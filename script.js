389ceb9467629c7ccbaf7444201657a2413a0f4d
patricklimax <patricklimax@outlook.com.br>
2022-09-13 12:51:11 -0300

    myname

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typing", {
-    strings: ["Desenvolvedor Web"],
+    strings: ["dEsENVOLVEDoR WeB"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
